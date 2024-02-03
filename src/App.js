d7f91fc799e53e5a7160f2c74534db797a4eb711
MuhammadNazir <naziraliashraf@gmail.com>
2024-02-03 14:56:33 +0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import DataFetchingComponent from './DataFetchingComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DataFetchingComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
